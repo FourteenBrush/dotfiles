747239fa6c8437728a9b74417a3158ec28efa440
FourteenBrush <74827262+FourteenBrush@users.noreply.github.com>
2024-08-10 16:55:03 +0200

    Update config.fish

--- config.fish
+++ config.fish
@@ -7,14 +7,31 @@ fish_add_path /usr/local/bin/visualvm/bin
 fish_add_path /usr/local/bin/odin
 fish_add_path /usr/local/bin/ols
 fish_add_path /usr/local/bin/c3c
+fish_add_path /usr/local/bin/floorp
 
 set fish_color_valid_path
-set -Ux JAVA_HOME /usr/lib/jvm/openjdk-21
+set -Ux JAVA_HOME /usr/lib/jvm/java-21-openjdk-amd64
 set -Ux SUDO_EDITOR nvim
 set -Ux EDITOR nvim
+set -Ux ODIN_ROOT (odin root)
 
 zoxide init fish | source
 
 function jdgui
     java -jar /usr/local/bin/jars/jdgui.jar $argv & disown
 end
+
+function update_odin
+    pushd .
+    cd /usr/local/bin/odin
+    git pull
+    ./update_odin release-native
+
+    cd /usr/local/bin/ols
+    git pull
+    ./build
+    popd
+    popd
+end
+
+alias bat batcat
