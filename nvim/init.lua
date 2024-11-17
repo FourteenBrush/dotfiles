193e583b93c2c755a548fdbf17a0b66129067bc1
FourteenBrush <74827262+FourteenBrush@users.noreply.github.com>
2024-11-17 18:06:48 +0100

    Fix autocmds and things

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,36 +5,39 @@ vim.g.maplocalleader        = ' '
 vim.g.nvim_tree_group_empty = 1
 vim.g.base46_cache          = vim.fn.stdpath 'data' .. '/nvchad/base46/'
 
-local opt                = vim.opt
+local opt                   = vim.opt
+opt.syntax                  = 'enable'
+opt.background              = 'dark'
+opt.mouse                   = 'a'
+opt.number                  = true
 
-opt.syntax               = 'enable'
-opt.background           = 'dark'
-opt.mouse                = 'a'
-opt.number               = true
-opt.smarttab             = true
-opt.expandtab            = true
-opt.smartcase            = true
-opt.tabstop              = 4
-opt.shiftwidth           = 4
-opt.splitbelow           = true
-opt.splitright           = true
-opt.showmode             = false
-opt.foldmethod           = 'marker'
-opt.linebreak            = true
+opt.smarttab                = true
+-- use the appropriate number of spaces to insert a tab
+opt.expandtab               = true
+-- number of spaces that a tab counts for
+opt.tabstop                 = 4
+-- number of spaces to use for each step of (auto)indent
+opt.shiftwidth              = 4
+opt.smartcase               = true
+opt.splitbelow              = true
+opt.splitright              = true
+opt.showmode                = false
+opt.foldmethod              = 'marker'
+opt.linebreak               = true
 -- opt.infrocommand           = 'split'
 -- number of lines to keep above and below cursor when f.e. jumping
-opt.scrolloff            = 2
+opt.scrolloff               = 2
 -- case insentive search unless \C or capital in search
-opt.ignorecase           = true
-opt.smartcase            = true
--- set highlight on search
-opt.hlsearch             = true
+opt.ignorecase              = true
+opt.smartcase               = true
+-- persistent undo
+opt.undofile                = true
 
 --------------------------------------------------
 -- Plugins
 --------------------------------------------------
 
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath              = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
 --- @diagnostic disable-next-line: undefined-field
 if not vim.uv.fs_stat(lazypath) then
@@ -51,12 +54,11 @@ end
 opt.rtp:prepend(lazypath)
 
 require('lazy').setup {
-  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate', event = { 'BufReadPre', 'BufNewFile' } },
+  { 'nvim-treesitter/nvim-treesitter',  build = ':TSUpdate',                                     event = { 'BufReadPre', 'BufNewFile' } },
   { 'neovim/nvim-lspconfig' },
   { 'williamboman/mason.nvim' },
   { 'williamboman/mason-lspconfig.nvim' },
-  --{ 'Shatur/neovim-ayu' },
-  { 'hrsh7th/nvim-cmp', event = { 'InsertEnter', 'CmdlineEnter', 'BufReadPost' } }, -- autocompletion
+  { 'hrsh7th/nvim-cmp',                 event = { 'InsertEnter', 'CmdlineEnter', 'BufReadPost' } }, -- autocompletion
   {
     'L3MON4D3/LuaSnip',
     build = 'make install_jsregexp',
@@ -70,21 +72,31 @@ require('lazy').setup {
   {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-file-browser.nvim' },
   },
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
-  { 'akinsho/toggleterm.nvim', config = true },
+  { 'akinsho/toggleterm.nvim',                       config = true },
   { 'https://github.com/junegunn/vim-easy-align.git' },
-  { 'NvChad/base46', build = function ()
-      require('base46').load_all_highlights()
-    end
+  { 'NvChad/base46', build = function()
+    require('base46').load_all_highlights()
+  end,
   },
-  { 'NvChad/ui', lazy = false },
+  { 'NvChad/ui',              lazy = false },
   { 'lewis6991/gitsigns.nvim' },
-  --{ 'ayu-theme/ayu-vim' },
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    keys = {
+      { '<Leader>s', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end },
+      { '<Leader>S', mode = { 'n', 'x', 'o' }, function() require('flash').treesitter() end },
+      { '<Leader>R', mode = { 'o', 'x' },      function() require('flash').treesitter_search() end },
+    },
+  },
+  { 'windwp/nvim-ts-autotag' },
+  { 'stevearc/conform.nvim' },
 }
 
 --------------------------------------------------
@@ -112,27 +124,45 @@ local function new_maker (filepath, bufnr, opts)
     end
   }:sync()
 end
-]]--
+]] --
 
 local telescope = require('telescope')
+local actions = require('telescope.actions')
 telescope.setup {
   defaults = {
     -- buffer_previewer_maker = new_maker,
     file_ignore_patterns = { 'node_modules', '.git/' },
+    mappings = {
+      i = {
+        ['<C-h>'] = actions.select_horizontal,
+      },
+    },
   },
-  extensions_list = { 'themes' },
+  extensions_list = { 'themes', 'file_browser' },
+  -- pickers = {
+  --   find_files = {
+  --     mappings = {
+  --       i = {
+  --         ['<C-h>'] = 'select_horizontal',
+  --       },
+  --     },
+  --   },
+  -- },
 }
 -- TODO: use https://github.com/NvChad/ui/blob/dbdd2cfa7b6267e007e0b87ed7e2ea5c6979ef22/lua/telescope/_extensions/themes.lua#L82
-pcall(telescope.load_extension, 'themes')
+telescope.load_extension('themes')
+pcall(telescope.load_extension, 'file_browser')
 
 --- @diagnostic disable-next-line: missing-fields
 require('nvim-treesitter.configs').setup {
   ensure_installed = {
-    'odin', 'lua', 'javascript', 'c', 'cpp', 'vimdoc', 'java', 'comment', 'query', 'jsdoc',
-    'angular', 'rust', 'python', 'rust', 'javascript', 'diff', 'zig', 'go', 'bash',
+    'odin', 'lua', 'javascript', 'c', 'cpp', 'vimdoc', 'java', 'comment', 'query', 'jsdoc', 'dart',
+    'angular', 'rust', 'python', 'rust', 'javascript', 'diff', 'zig', 'go', 'bash', 'xml', 'typescript',
+    'css', 'fish', 'make',
   },
   highlight = { enable = true },
   indent = { enable = true },
+  -- default keybinding: gcc
   context_commentstring = {
     enable = true,
     autocmd = false,
@@ -153,43 +183,66 @@ local luasnip = require('luasnip')
 require('luasnip.loaders.from_vscode').lazy_load {}
 
 local cmp = require('cmp')
-local lspkind = require('lspkind')
 
 cmp.setup {
-  --[[
+  enabled = function()
+    local context = require('cmp.config.context')
+    -- keep command mode completion enabled when cursor is in a comment
+    if vim.api.nvim_get_mode().mode == 'c' then
+      return true
+    else
+      return not context.in_treesitter_capture('comment') and not context.in_syntax_group('Comment')
+    end
+  end,
   window = {
-    completion = cmp.config.window.bordered(),
+    -- completion = cmp.config.window.bordered(),
+    completion = {
+      winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,Search:None',
+      col_offset = -3,
+      side_padding = 0,
+    },
   },
-  --]]
   formatting = {
-    format = lspkind.cmp_format(),
+    fields = { 'kind', 'abbr', 'menu' },
+    format = function(entry, vim_item)
+      local kind = require('lspkind').cmp_format { mode = 'symbol_text', maxwidth = 50 } (entry, vim_item)
+      local strings = vim.split(kind.kind, '%s', { trimempty = true })
+      kind.kind = ' ' .. (strings[1] or '') .. ' '
+      kind.menu = '    (' .. (strings[2] or '') .. ')'
+      return kind
+    end
   },
-  sources = cmp.config.sources {
+  sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
-  },
+  }, { name = 'buffer' }),
   snippet = {
-    expand = function (args)
+    expand = function(args)
+      vim.notify("expanding snippet in snippet.expand")
       luasnip.lsp_expand(args.body)
     end,
   },
-  -- https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#api-2
   mapping = cmp.mapping.preset.insert {
-    -- FIXME: conflicting mapping with node_incremental
+    -- TODO: conflicting mapping with node_incremental
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-d>'] = cmp.mapping.scroll_docs(2),
     ['<C-u>'] = cmp.mapping.scroll_docs(-2),
-    ['<Tab>'] = cmp.mapping(function (fallback)
-      if cmp.visible() then
-        if luasnip.expandable() then
-          luasnip.expand()
-        else
-          cmp.confirm { select = true }
-        end
-      elseif luasnip.locally_jumpable(1) then
-        luasnip.jump(1)
-      else
+    ['Up'] = cmp.mapping.select_prev_item(),
+    ['Down'] = cmp.mapping.select_next_item(),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if not cmp.visible() then
         fallback()
+      elseif luasnip.expand_or_jumpable() then
+        local active = cmp.get_selected_entry()
+        -- only allow expanding or jumping if a snippet is actually selected in the menu
+        -- luasnip.expandable() somehow returns true if there is _any_ snippet in the completion menu
+        if active or luasnip.jumpable() then
+          cmp.confirm { select = true }
+        else
+          cmp.select_next_item()
+        end
+      else
+        cmp.confirm { select = true }
       end
     end),
     ['<S-Tab>'] = cmp.mapping(function(fallback)
@@ -202,12 +255,11 @@ cmp.setup {
       end
     end),
   },
-  { name = 'buffer' },
 }
 
 require('nvim-autopairs').setup { check_ts = true }
 
--- automatically add parenthesis after tabcompletion
+-- automatically add parenthesis on tabcompletion
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 cmp.event:on(
   'confirm_done',
@@ -215,17 +267,17 @@ cmp.event:on(
 )
 
 require('nvim-tree').setup {
-  view = { preserve_window_proportions = true },
+  view = { preserve_window_proportions = false },
   -- TODO: set back to false
   -- TODO: resize manually to proper size
-  actions = { open_file = { resize_window = true } },
+  actions = { open_file = { resize_window = false } },
   filters = { git_ignored = false },
   renderer = { group_empty = true, highlight_opened_files = 'name' },
 }
 
 -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close#ppwwyyxx
 vim.api.nvim_create_autocmd('QuitPre', {
-  callback = function ()
+  callback = function()
     local invalid_wins = {}
     local wins = vim.api.nvim_list_wins()
 
@@ -257,7 +309,7 @@ require('lualine').setup {
 require('toggleterm').setup {
   -- there's an mapping timeout issue with this approach
   -- open_mapping = '<leader>tt',
-  size = function (term)
+  size = function(term)
     if term.direction == 'horizontal' then
       return 15
     elseif term.direction == 'vertical' then
@@ -267,6 +319,15 @@ require('toggleterm').setup {
 }
 
 require('gitsigns').setup {}
+
+require('nvim-ts-autotag').setup {}
+
+local conform = require('conform')
+conform.setup {
+  formatters_by_ft = {
+    javascript = { 'prettierd', 'prettier', stop_after_first = true },
+  },
+}
 
 --------------------------------------------------
 -- Theme
@@ -280,11 +341,11 @@ require('ayu').setup {
   end,
 }
 vim.cmd.colorscheme('ayu-dark')
-]]--
+]] --
 -- vim.cmd [[ :hi NvimTreeFolderIcon guifg=#7094b4 ]]
 -- TODO: which of this crap do we actually need?
 local theme_integrations = {
-  'defaults', 'statusline', 'git', 'cmp', 'syntax', 'lsp', 'treesitter', 'nvimtree', 'statusline', 'telescope', 'term'}
+  'defaults', 'statusline', 'git', 'cmp', 'syntax', 'lsp', 'treesitter', 'nvimtree', 'statusline', 'telescope', 'term' }
 for _, integration in ipairs(theme_integrations) do
   dofile(vim.g.base46_cache .. integration)
 end
@@ -312,8 +373,9 @@ local builtin = require('telescope.builtin')
 
 keymap.set('n', '<leader>ff', '<cmd>:Telescope find_files hidden=true<cr>')
 keymap.set('n', '<leader>fi', '<cmd>:Telescope find_files hidden=true theme=ivy<cr>')
+keymap.set('n', '<leader>b', '<cmd>:Telescope file_browser<cr>')
 keymap.set('n', '<leader>fv', '<cmd>:Telescope find_files hidden=true layout_strategy=vertical<cr>')
-keymap.set('n', '<leader>fc', '<cmd>:Telescope current_buffer_fuzzy_find<cr>')
+keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find)
 keymap.set('n', '<leader>fg', builtin.live_grep)
 keymap.set('n', '<leader>gi', '<cmd>:Telescope live_grep theme=ivy<cr>')
 keymap.set('n', '<leader>gv', '<cmd>:Telescope live_grep layout_strategy=vertical<cr>')
@@ -335,8 +397,8 @@ keymap.set('n', '<Leader><Right>', tabufline.next)
 -- tt :vnew term://fish
 
 keymap.set('n', '<leader>tt', '<cmd>exe v:count . "ToggleTerm"<cr>')
-keymap.set('n', '<leader>tv', '<cmd>:ToggleTerm direction=vertical<cr>')
-keymap.set('n', '<leader>th', '<cmd>:ToggleTerm direction=horizontal<cr>')
+keymap.set('n', '<leader>tv', '<cmd>exe v:count . "ToggleTerm direction=vertical"<cr>')
+keymap.set('n', '<leader>th', '<cmd>exe v:count . "ToggleTerm direction=horizontal"<cr>')
 keymap.set('n', '<leader>tf', '<cmd>exe v:count . "ToggleTerm direction=float"<cr>')
 
 keymap.set('n', '<leader>e', vim.diagnostic.open_float)
@@ -367,7 +429,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
     keymap.set('n', '<C-;>', vim.lsp.buf.code_action, opts)
-    keymap.set('n', 'gr', vim.lsp.buf.rename, opts)
+    keymap.set('n', 'gr', require('nvchad.lsp.renamer'), opts)
     keymap.set('n', 'gu', vim.lsp.buf.references, opts)
     keymap.set('n', '<leader>fd', function()
       vim.lsp.buf.format { async = true }
@@ -376,14 +438,19 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 
-vim.api.nvim_create_autocmd('FileType', {
+vim.api.nvim_create_autocmd('BufEnter', {
   group = lspgroup,
-  pattern = 'html,xhtml,javascript,javascriptreact,typescript,typescriptreact,css,scss,lua',
-  callback = function()
-    local opts = { scope = 'local' }
-    vim.api.nvim_set_option_value('tabstop', 2, opts)
-    vim.api.nvim_set_option_value('shiftwidth', 2, opts)
-    vim.api.nvim_set_option_value('softtabstop', 2, opts)
+  pattern = { '*.html', '*.xhtml', '*.js', '*.jsx', '*.ts', '*.tsx', '*.css', '*.css', '*.lua' },
+  callback = function(event)
+    local formatters_for_buf = conform.list_formatters(event.buf)
+    if #formatters_for_buf > 0 then
+      return -- do not override configs from formatters
+    end
+
+    for _, key in ipairs({ 'tabstop', 'shiftwidth', 'softtabstop' }) do
+      vim.bo[event.buf][key] = 2
+      -- vim.api.nvim_set_option_value(key, 2, { scope = 'local' })
+    end
   end,
 })
 
@@ -399,19 +466,21 @@ require('mason').setup {
   },
 }
 
-local mason_lspconfig = require('mason-lspconfig')
 local lspconfig = require('lspconfig')
 
 -- setup manually installed lsp servers
 lspconfig.ols.setup {}
 
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
 local handlers = {
-  function (server_name)
-    -- TODO: provide on_attach and capabilities?
-    lspconfig[server_name].setup {}
+  function(server_name)
+    lspconfig[server_name].setup {
+      capabilities = capabilities,
+    }
   end,
 
-  ['lua_ls'] = function ()
+  ['lua_ls'] = function()
     lspconfig.lua_ls.setup {
       settings = {
         Lua = {
@@ -429,7 +498,8 @@ local handlers = {
     }
   end
 }
-mason_lspconfig.setup {
+
+require('mason-lspconfig').setup {
   automatic_installation = true,
   handlers = handlers,
 }
