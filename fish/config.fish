229f455800215c32d32af0afb2e408af659c64f5
FourteenBrush <74827262+FourteenBrush@users.noreply.github.com>
2025-04-08 13:03:14 +0200

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -10,15 +10,28 @@ fish_add_path /usr/local/bin/c3c
 fish_add_path /usr/local/bin/floorp
 
 set fish_color_valid_path
+set fish_greeting
+
 set -Ux JAVA_HOME /usr/lib/jvm/java-21-openjdk-amd64
 set -Ux SUDO_EDITOR nvim
 set -Ux EDITOR nvim
 set -Ux ODIN_ROOT (odin root)
+set -x MANPAGER "nvim +Man!"
+
+alias b bluetoothctl
+abbr gc "git commit"
+abbr gcm 'git commit -m "'
+
+fish_vi_key_bindings
 
 zoxide init fish | source
 
 function jdgui
     java -jar /usr/local/bin/jars/jdgui.jar $argv & disown
+end
+
+function package_build_shell
+    nix-shell -E "with import <nixpkgs> {}; callPackage $argv[1] {}"
 end
 
 function update_odin
